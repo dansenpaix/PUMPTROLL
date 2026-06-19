e9703254d65a09a7bc46e472462e5d62a6af7ab3
ddanny16 <102356223+ddanny16@users.noreply.github.com>
2026-06-19 23:46:52 +0100

    Fix Tailwind v4 build error for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import netlify from '@netlify/vite-plugin-tanstack-start'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
+    tailwindcss(),
     tanstackStart(),
-    netlify(),                  
-  ],
+    process.env.NODE_ENV === 'production' ? netlify() : null,
+  ].filter(Boolean),
+
+  css: {
+    lightningcss: false,
+    postcss: {
+      plugins: [],
+    },
+  },
 })
